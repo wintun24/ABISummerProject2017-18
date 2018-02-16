18f78a8e014180d715db7022b88a1cc7f5fcd4a9
rorymckay2 <34292467+rorymckay2@users.noreply.github.com>
2018-02-16 15:32:21 +1300

    Add files via upload

---
+++ PlacentaModels/vfileConvert.py
@@ -0,0 +1,29 @@
+#Read in .exnode file
+exnodeFile = open('DarcyResults.part0.exnode', 'r')
+exnode = exnodeFile.readlines()
+exnodeFile.close()
+nodeStarts = range(23,len(exnode)-9,14)
+
+nodes = []
+nodeNum = 1
+
+for i in nodeStarts:
+  nodeV = []
+  nodeV.append(int(nodeNum))
+  nodeV.append(float(exnode[i]))
+  nodeV.append(float(exnode[i+1]))
+  nodeV.append(float(exnode[i+2]))
+  nodes.append(nodeV)
+  nodeNum = nodeNum+1
+
+#Write out as file with [node, v1, v2, v3]
+vOutput = open('vData.txt', 'w')
+
+for node in nodes:
+  line = str(node[0]) + ' ' + str(1) + ' ' + str(node[1]) +'\n'
+  vOutput.write(line)
+  line = str(node[0]) + ' ' + str(2) + ' ' + str(node[2]) +'\n'
+  vOutput.write(line)
+  line = str(node[0]) + ' ' + str(3) + ' ' + str(node[3]) +'\n'
+  vOutput.write(line)
+vOutput.close()
